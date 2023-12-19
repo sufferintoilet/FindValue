095fb7e5b038550526694bd033774daac9c2dc2d
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-19 22:24:29 +0900

    find name

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        string[] nameList={"Nam", "Steve", "Doyle", "Bac", "Sikorsky", "Lam", "Loc", "Akira", "Timmy"};
+        Console.WriteLine("Enter name");
+        string name = Console.ReadLine();
+        bool isTrue = false;
+        int i = 0;
+        while (i <= nameList.Length-1)
+        {
+            if (nameList[i].Equals(name))
+            {
+                Console.WriteLine($"Co ten trong mang va vi tri {i+1}");
+                isTrue=true;
+                break;
+            }
+            i++;
+        }
+        if (!isTrue)
+        {
+            Console.WriteLine("no name");
+        }
+    }
+}
